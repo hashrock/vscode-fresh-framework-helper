7b93d465172148e9366602e6794588aed8f8b351
hashrock <hashrock@users.noreply.github.com>
2023-09-03 05:52:09 +0900

    refactor

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import * as snippets from "./snippets";
 
 export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
@@ -52,76 +53,12 @@ export function activate(context: vscode.ExtensionContext) {
         token: vscode.CancellationToken,
         context: vscode.CompletionContext,
       ) {
-        const simpleRoute = new vscode.CompletionItem("Fresh - Simple Route");
-        simpleRoute.insertText = new vscode.SnippetString(
-          [
-            "export default function ${1:Page}(props: PageProps) {",
-            "	return (",
-            "		<main>",
-            "			<h1>About</h1>",
-            "			<p>This is the about page.</p>",
-            "		</main>",
-            "	);",
-            "});",
-          ].join("\n"),
-        );
-
-        const customHandlers = new vscode.CompletionItem(
-          "Fresh - Custom Handlers",
-        );
-        customHandlers.insertText = new vscode.SnippetString([
-          "export const handler: Handlers = {",
-          "	async GET(_req, ctx) {",
-          "    return await ctx.render();",
-          "  },",
-          "};",
-        ].join("\n"));
-
-        const layout = new vscode.CompletionItem(
-          "Fresh - Layouts",
-        );
-
-        layout.insertText = new vscode.SnippetString([
-          "export default function Layout({ Component, state }: LayoutProps) {",
-          "	// do something with state here",
-          "	return (",
-          '		<div class="layout">',
-          "			<Component />",
-          "		</div>",
-          "	);",
-          "}",
-        ].join("\n"));
-
-        const defineRoute = new vscode.CompletionItem(
-          "Fresh - Define Route",
-        );
-        defineRoute.insertText = new vscode.SnippetString([
-          "export default defineRoute(async (req, ctx) => {",
-          "	return (",
-          "		<div></div>",
-          "	);",
-          "});",
-        ].join("\n"));
-
-        const defineLayout = new vscode.CompletionItem(
-          "Fresh - Define Layout",
-        );
-        defineLayout.insertText = new vscode.SnippetString([
-          "export default defineLayout(async (req, ctx) => {",
-          "	return (",
-          "		<div>",
-          "			<ctx.Component />",
-          "		</div>",
-          "	);",
-          "});",
-        ].join("\n"));
-
         return [
-          simpleRoute,
-          customHandlers,
-          layout,
-          defineRoute,
-          defineLayout,
+          snippets.simpleRoute,
+          snippets.customHandlers,
+          snippets.layout,
+          snippets.defineRoute,
+          snippets.defineLayout,
         ];
       },
     },
