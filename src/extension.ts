ac76f617a35038aa216cca652e6d9f38c8c351d9
hashrock <hashrock@users.noreply.github.com>
2023-09-03 05:27:11 +0900

    add route file creation

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,48 @@
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    "fresh-snippets.generateRoute",
+    async (uri: vscode.Uri) => {
+      const fileName = await vscode.window.showInputBox({
+        prompt: "Enter file name",
+        placeHolder: "index.tsx",
+        value: "index.tsx",
+      });
+
+      const newFile = uri.fsPath + "/" + fileName;
+      // check if file exists
+      try {
+        const fileExists = await vscode.workspace.fs.stat(
+          vscode.Uri.file(newFile),
+        );
+        if (fileExists) {
+          vscode.window.showErrorMessage("File already exists");
+          return;
+        }
+      } catch (error) {
+        // do nothing
+      }
+
+      vscode.workspace.fs.writeFile(
+        vscode.Uri.file(newFile),
+        new Uint8Array(Buffer.from([
+          "import { defineRoute } from '$fresh/server.ts';",
+          "",
+          "export default defineRoute(async (req, ctx) => {",
+          "	return (",
+          "		<div></div>",
+          "	);",
+          "});",
+        ].join("\n"))),
+      );
+
+      vscode.window.showInformationMessage("File Created");
+    },
+  );
+
+  context.subscriptions.push(disposable);
+
   const provider1 = vscode.languages.registerCompletionItemProvider(
     "typescriptreact",
     {
@@ -79,7 +121,7 @@ export function activate(context: vscode.ExtensionContext) {
           customHandlers,
           layout,
           defineRoute,
-					defineLayout,
+          defineLayout,
         ];
       },
     },
