583f89e0b1390cbf027d0596443dfb2a61c39cca
hashrock <hashrock@users.noreply.github.com>
2023-09-03 07:35:14 +0900

    feat: add classname generation

--- src/generate.ts
+++ src/generate.ts
@@ -23,6 +23,23 @@ async function writeFile(newFile: string, body: string) {
   vscode.window.showInformationMessage("File Created");
 }
 
+function kebabToCamelCase(str: string) {
+  return str.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
+}
+
+function snakeToCamelCase(str: string) {
+  return str.replace(/_([a-z])/g, (g) => g[1].toUpperCase());
+}
+
+function capitalizeFirstLetter(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function camelizeWhatever(str: string) {
+  return capitalizeFirstLetter(snakeToCamelCase(kebabToCamelCase(str)));
+}
+
+
 const routes = [
   {
     label: "Simple JSX Page",
@@ -39,7 +56,7 @@ const routes = [
     label: "Dynamic route",
     body: `import { PageProps } from "$fresh/server.ts";
 
-export default function GreetPage(props: PageProps) {
+export default function __FILENAME__(props: PageProps) {
   const { name } = props.params;
   return (
     <main>
@@ -64,25 +81,29 @@ const asyncRoutes = [
   {
     label: "Async component route",
     body:
-      `export default async function MyPage(req: Request, ctx: RouteContext) {
+      `import { RouteContext } from "$fresh/server.ts";
+  
+  export default async function __FILENAME__(req: Request, ctx: RouteContext) {
   // const value = await loadFooValue();
   return <p>foo is: {1}</p>;
 }`,
   },
   {
     label: "Mixed handler and component route",
-    body: `interface Data {
+    body: `import { Handlers, PageProps } from "$fresh/server.ts";
+
+interface Data {
   foo: number;
 }
 
 export const handler: Handlers<Data> = {
-  async GET(req, ctx) {
+  async GET(_req, ctx) {
     // const value = await loadFooValue();
-    ctx.render({ foo: 1 });
+    return ctx.render({ foo: 1 });
   },
 };
 
-export default function MyPage(props: PageProps<Data>) {
+export default function __FILENAME__(props: PageProps<Data>) {
   return <p>foo is: {props.data.foo}</p>;
 }`,
   },
@@ -91,7 +112,8 @@ export default function MyPage(props: PageProps<Data>) {
     body: `import { defineRoute } from "$fresh/server.ts";
 
 export default defineRoute(async (req, ctx) => {
-  const data = await loadData();
+  // const data = await loadData();
+  const data = { name: "World" };
 
   return (
     <div class="page">
@@ -138,6 +160,8 @@ export async function generateRoute(uri: vscode.Uri) {
     return;
   }
 
+  body = body.replace(/__FILENAME__/g, camelizeWhatever(fileName.replace(".tsx", "")));
+
   try {
     await writeFile(newFile, body);
   } catch (error: unknown) {
@@ -159,6 +183,10 @@ async function generateFile(uri: vscode.Uri, body: string, defaultFileName = "in
   }
 
   const newFile = uri.fsPath + "/" + fileName;
+
+  const nameForClass = camelizeWhatever(fileName.split(".")[0]);
+  body = body.replace(/__FILENAME__/g, nameForClass);
+
 
   try {
     await writeFile(newFile, body);
@@ -185,10 +213,10 @@ export default function Layout({ Component, state }: LayoutProps) {
 }
 
 export async function generateComponent(uri: vscode.Uri) {
-  const body = `export function MyComponent() {
+  const body = `export function __FILENAME__() {
   return (
     <div>
-      <h1>MyComponent</h1>
+      <h1>__FILENAME__</h1>
     </div>
   );
 }`;
@@ -199,11 +227,11 @@ export async function generateComponent(uri: vscode.Uri) {
 export async function generateIsland(uri: vscode.Uri) {
   const body = `import type { Signal } from "@preact/signals";
 
-interface CounterProps {
+interface __FILENAME__Props {
   count: Signal<number>;
 }
 
-export default function Counter(props: CounterProps) {
+export default function __FILENAME__(props: __FILENAME__Props) {
   return (
     <div>
       <button onClick={() => props.count.value -= 1}>-1</button>
