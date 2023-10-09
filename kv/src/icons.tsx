b081c9323de98248cac3e4edb8fe075d90427b49
hashrock <hashrock@users.noreply.github.com>
2023-10-10 00:52:29 +0900

    fix jsx things

--- kv/src/icons.tsx
+++ kv/src/icons.tsx
@@ -12,11 +12,11 @@ export function IconDatabase(props: React.SVGProps<SVGSVGElement>) {
       width={props.width ?? "24"}
       height={props.height ?? "24"}
       viewBox="0 0 24 24"
-      stroke-width="2"
+      strokeWidth="2"
       stroke="currentColor"
       fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
@@ -52,11 +52,11 @@ export function IconPlus(props: React.SVGProps<SVGSVGElement>) {
       width={props.width ?? "24"}
       height={props.height ?? "24"}
       viewBox="0 0 24 24"
-      stroke-width="3"
+      strokeWidth="3"
       stroke="currentColor"
       fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
       <path d="M12 5l0 14"></path>
